4625364bbd9606e3cecfcba1fb60d3790bc64da5
Attila <atti80@gmail.com>
2025-02-28 16:56:01 +0100

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="m-4 font-roboto">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton mode="modal">
-          <Button>Sign Upp</Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle></ModeToggle>
-    </div>
-  );
+  return <div className="m-4 font-roboto"></div>;
 }
